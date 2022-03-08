8c3b19ba3a4a8ee2d85fe46315216bbadd7cd99e
Roqueheli <roqueheliarenas@gmail.com>
2022-03-08 12:34:51 -0300

    cambios al navigator

--- src/components/Header/Navigator.jsx
+++ src/components/Header/Navigator.jsx
@@ -5,11 +5,11 @@ function Navigator() {
   return (
     <nav>
         <ul>
-            <li><a href="#hamburguers" activeclassname='active'>Hamburguesas</a></li>
-            <li><a href="#hotdogs" activeclassname='active'>Perros Calientes</a></li>
-            <li><a href="#arepas" activeclassname='active'>Arepas</a></li>
-            <li><a href="#drinks" activeclassname='active'>Bebidas</a></li>
-            <li><a href="#extras" activeclassname='active'>Adicionales</a></li>
+          <li><a href="#hamburguers" activeclassname='active'>Hamburguesas</a></li>
+          <li><a href="#hotdogs" activeclassname='active'>Perros Calientes</a></li>
+          <li><a href="#arepas" activeclassname='active'>Arepas</a></li>
+          <li><a href="#drinks" activeclassname='active'>Bebidas</a></li>
+          <li><a href="#extras" activeclassname='active'>Adicionales</a></li>
         </ul>
     </nav>
   )
